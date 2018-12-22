48688478a35cd86859411db1c59ecc37345809ed
Onyekachi Okereke <robocopkaka@gmail.com>
2018-12-22 15:08:50 +0100

    set up eslint

--- app/client/src/components/centers/container/Centers.jsx
+++ app/client/src/components/centers/container/Centers.jsx
@@ -3,33 +3,33 @@ import axios from 'axios';
 import CentersList from '../presentation/CentersList';
 
 class Centers extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          centers: []
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      centers: []
+    };
+  }
 
-    componentDidMount() {
-      axios.get('/api/v1/centers')
-          .then(response => {
-              // console.log(response.data.data)
-              this.setState({
-                  centers: response.data.data.centers
-              });
-          })
-          .catch(error =>{
-              console.log(error)
-          });
-    }
+  componentDidMount() {
+    axios.get('/api/v1/centers')
+      .then((response) => {
+        // console.log(response.data.data);
+        this.setState({
+          centers: response.data.data.centers
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    render() {
-      return (
-        <div className="columns is-multiline">
-          <CentersList centers={this.state.centers}  />
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="columns is-multiline">
+        <CentersList centers={this.state.centers} />
+      </div>
+    );
+  }
 }
 
 export default Centers;
