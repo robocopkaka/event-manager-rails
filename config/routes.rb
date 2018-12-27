5fb2cdff0215aab079dcc3ee32dc6c7a1bbadd51
Onyekachi Okereke <robocopkaka@gmail.com>
2018-12-27 01:07:35 +0100

    feat(auth): users should be able to signup and login
    - users should be able to signup
    - users should be able to login and receive a token
    
    why: to help protect restricted routes
    
    [Starts #162706013]

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :centers
+      resources :users
+      post 'user_token' => 'user_token#create'
     end
   end
 end
