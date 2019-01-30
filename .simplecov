28c45017b7843af64abab7cc39293917b65dedd3
Onyekachi Okereke <robocopkaka@gmail.com>
2019-01-30 18:29:19 +0100

    chore(ci): setup continuous integration
    - display coverage information locally
    
    why: so that I can see covered lines without having to go to Coveralls
    
    [Finishes #163586370]

--- .simplecov
+++ .simplecov
@@ -1,1 +1,6 @@
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_group "Models", "app/models"
+  add_group "V1 Controllers", "app/controllers/api/v1"
+  add_group "Helpers", "app/helpers"
+  add_group "Channels", "app/channels"
+end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :user do
     name { Faker::Name.first_name }
-    email { "#{Faker::Name.first_name}@events.com" }
+    email { "#{Faker::Name.first_name}-#{Faker::Name.last_name}@events.com" }
     admin { false }
     password { 'password' }
     password_confirmation { 'password' }
